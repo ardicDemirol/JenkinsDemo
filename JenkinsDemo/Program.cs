5de170cfcd1e5a32179e58baa0275c197b0ca6d7
ardicDemirol <ardc.dmrol@gmail.com>
2024-11-11 15:21:02 +0300

    launchSetting.json v6

--- JenkinsDemo/Program.cs
+++ JenkinsDemo/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://*:1000");
+//builder.WebHost.UseUrls("http://*:1000");
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
